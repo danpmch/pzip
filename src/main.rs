b57e078e14a0c6e6ac879a5e0e5ce4c0b88b07f8
Dan McHenry <danpmch@gmail.com>
2020-09-04 16:50:54 -0400

    Encode/decode with a u32 length header to allow trimming the padding bytes

--- src/main.rs
+++ src/main.rs
@@ -29,34 +29,41 @@ fn encode(filename: &String) {
     let mut f = File::open(filename)
         .expect("Could not open test.txt");
 
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
+    let mut content_bytes: Vec<u8> = Vec::new();
+    f.read_to_end(&mut content_bytes)
         .expect("Could not read file contents");
 
-    println!("{}", contents);
+    println!("Total bytes in file: {}", content_bytes.len());
 
-    let mut bytes = contents.into_bytes();
+    let mut v = Vec::<u8>::new();
+    let size: u32 = content_bytes
+        .len()
+        .try_into()
+        .expect("Error, file is too big for the Rust image library to handle");
+    v.extend(&size.to_be_bytes());
+    v.extend(content_bytes);
 
-    let (width, height) = {
-        let total_pixels = ((bytes.len() as f64) / 3.0).ceil();
-        let w: u32 = total_pixels.sqrt() as u32;
-        let h: u32 = (total_pixels as u32) / w +
-            if total_pixels as u32 % w > 0 { 1 } else { 0 };
-        (w, h)
+    let (width, height): (u32, u32) = {
+        let total_pixels = ((v.len() as f64) / 3.0).ceil();
+        let w = total_pixels.sqrt().floor();
+        let h = (total_pixels / w).ceil();
+        (w as u32, h as u32)
     };
 
-    println!("Total bytes to write {}", bytes.len());
+    println!("Total bytes to write {}", v.len());
     println!("Computed Width, Height: ( {}, {} )", width, height);
     println!("Total image bytes: {}", width * height * 3);
 
-    let missing_bytes = (width * height * 3) - (bytes.len() as u32);
-    bytes.extend(vec![0; missing_bytes.try_into().unwrap()].iter());
+    let missing_bytes = (width * height * 3) - (v.len() as u32);
+
+    v.extend(vec![0; missing_bytes.try_into().unwrap()].iter());
+    //println!("{:x?}", v);
 
     let output_file = File::create(filename.to_owned() + ".png")
         .expect("Could not open output file test.png");
     let png = PngEncoder::new(output_file);
     png.encode(
-      &bytes,
+      &v,
       width,
       height,
       image::ColorType::Rgb8
@@ -67,17 +74,26 @@ fn decode(filename: &String) {
     let f = File::open(filename)
         .expect("Could not open test.txt");
     let png = PngDecoder::new(f).expect("Could not decode file as PNG");
-    let mut contents = vec![0 as u8; png.total_bytes().try_into().unwrap()];
+    let mut contents = vec![0u8; png.total_bytes().try_into().unwrap()];
 
-    println!("Total image bytes: {}", png.total_bytes());
-    println!("Size of buffer: {}", contents.len());
+    //println!("Total image bytes: {}", png.total_bytes());
+    //println!("Size of buffer: {}", contents.len());
 
     png.read_image(&mut contents)
         .expect("Error reading image");
+    //println!("{:x?}", contents);
+
+    let size_bytes: [u8; 4] = contents[0..4]
+        .try_into()
+        .expect("Error, could not read data length");
+    let size: usize = u32::from_be_bytes(size_bytes).try_into().unwrap();
+    //println!("Size header: {}", size);
+    let data = &contents[4..(4 + size)];
+    //println!("{:x?}", data);
 
     let mut output_file = File::create(filename.to_owned() + ".decoded")
         .expect("Error opening output file");
-    output_file.write(&contents).expect("Error writing output file");
+    output_file.write(&data).expect("Error writing output file");
 }
 
 
