56a25d598ea58d51fd2a20453385ce33d018df87
Dan McHenry <danpmch@gmail.com>
2020-09-04 17:15:42 -0400

    Added comments

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,18 @@ fn encode(filename: &String) {
 
     println!("Total bytes in file: {}", content_bytes.len());
 
+    /* We have to have enough bytes to fill out all the pixels in the image.
+     * The easy way to deal with this would be to make the image 1px high
+     * and just encode the entire input file as a single row of the image,
+     * but that isn't very much fun to look at. What I'm doing instead is making
+     * the image roughly square, which means that sometimes you need to pad the
+     * data with extra bytes to fill out the last row with pixel data.
+     *
+     * This introduces a problem, because when decoding the image you need to
+     * know how many bytes represent the actual input file so you can discard
+     * the extra padding. I solve this by adding a u32 to the front of the data
+     * that encodes the length of the input file in bytes.
+     */
     let mut v = Vec::<u8>::new();
     let size: u32 = content_bytes
         .len()
@@ -45,7 +57,13 @@ fn encode(filename: &String) {
 
     let (width, height): (u32, u32) = {
         let total_pixels = ((v.len() as f64) / 3.0).ceil();
+        /* Here I make the image roughly square by basing the width
+         * off the square root. Because I'm taking the floor of the
+         * sqrt the image will always be taller than it is wide
+         */
         let w = total_pixels.sqrt().floor();
+
+        // take the ceil to ensure all the data will fit
         let h = (total_pixels / w).ceil();
         (w as u32, h as u32)
     };
@@ -54,8 +72,9 @@ fn encode(filename: &String) {
     println!("Computed Width, Height: ( {}, {} )", width, height);
     println!("Total image bytes: {}", width * height * 3);
 
+    // Here I add extra null bytes to pad the data to the size of the
+    // image.
     let missing_bytes = (width * height * 3) - (v.len() as u32);
-
     v.extend(vec![0; missing_bytes.try_into().unwrap()].iter());
     //println!("{:x?}", v);
 
@@ -83,11 +102,13 @@ fn decode(filename: &String) {
         .expect("Error reading image");
     //println!("{:x?}", contents);
 
+    // read the first u32 that tells the length of the data
     let size_bytes: [u8; 4] = contents[0..4]
         .try_into()
         .expect("Error, could not read data length");
     let size: usize = u32::from_be_bytes(size_bytes).try_into().unwrap();
     //println!("Size header: {}", size);
+
     let data = &contents[4..(4 + size)];
     //println!("{:x?}", data);
 
